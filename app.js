b891e869e91bd44e0880ac95b27245ed47f04b89
Mevorah <berrebimevo@gmail.com>
2024-11-25 14:05:44 +0200

    Initialize app_A and Connect to Mongo

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv").config();
 const port = process.env.PORT;
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DB_CONNECT);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+    console.log("Connected to the database");
+    });
 
 const posts_routes = require("./routes/posts_routes");  
 app.use("/posts", posts_routes);
