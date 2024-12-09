014f7316b2df1fa7375e11b72b2e4979e21ecf3e
Mevorah Berrebi <berrebimevo@gmail.com>
2024-12-09 14:50:43 +0200

    open test file

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ const port = process.env.PORT;
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DB_CONNECT);
 const db = mongoose.connection;
-db.on("error", (error) => console.log(error));
+db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to the database"));
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const postsRoutes = require("./routes/posts_routes");  
-app.use("/posts", postsRoutes);
+const postsRoute = require("./routes/posts_route");  
+app.use("/posts", postsRoute);
 
 app.listen(port, () => {
     console.log(`Exemple app listening at http://localhost:${port}`);
